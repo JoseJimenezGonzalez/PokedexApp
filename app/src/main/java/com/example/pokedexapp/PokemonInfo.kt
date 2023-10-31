71f4b746b7d600eb13dd34aa7903cb7eb28daa2d
JoseJimenezGonzalez <josexisco1993@gmail.com>
2023-10-31 16:59:24 +0100

    commit cremita

--- app/src/main/java/com/example/pokedexapp/PokemonInfo.kt
+++ app/src/main/java/com/example/pokedexapp/PokemonInfo.kt
@@ -8,16 +8,16 @@ data class PokemonInfo(
     val nombre: String,
     val peso: Double,
     val altura: Double,
-    val tipos: String, // Cambiado a String
-    val estadisticas: String // Cambiado a String
+    val tipos: String,
+    val estadisticas: String
 ) : Parcelable {
     constructor(parcel: Parcel) : this(
         parcel.readString() ?: "",
         parcel.readString() ?: "",
         parcel.readDouble(),
         parcel.readDouble(),
-        parcel.readString() ?: "", // Cambiado a String
-        parcel.readString() ?: "" // Cambiado a String
+        parcel.readString() ?: "",
+        parcel.readString() ?: ""
     )
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
@@ -25,8 +25,8 @@ data class PokemonInfo(
         parcel.writeString(nombre)
         parcel.writeDouble(peso)
         parcel.writeDouble(altura)
-        parcel.writeString(tipos) // Cambiado a String
-        parcel.writeString(estadisticas) // Cambiado a String
+        parcel.writeString(tipos)
+        parcel.writeString(estadisticas)
     }
 
     override fun describeContents(): Int {
